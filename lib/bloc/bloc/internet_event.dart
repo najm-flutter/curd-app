a094f4d2d6a13c9e054cec5d716960d28926d259
najm <najm.gmail.com>
2024-04-26 15:27:10 +0300

    defult

--- lib/bloc/bloc/internet_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'internet_bloc.dart';
-
-@immutable
-sealed class InternetEvent {}
-class InternetConectEvent extends InternetEvent{}
-class InternetNoConectEvent extends InternetEvent{}
--- lib/layear/screens/home.dart
+++ lib/layear/screens/home.dart
@@ -1,4 +1,3 @@
-import 'package:block_test/bloc/bloc/internet_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -8,27 +7,10 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<InternetBloc, InternetState>(
-        builder: (context, state) {
-          if (state is InternetConectState) {
-            print("object") ;
-                return Center(
-            child: Text(
-                    state.internetState),
-          );
-          }else if (state is InternetNoConectState) {
-            print("objectfgkflkg") ;
-                return Center(
-            child: Text(
-                    state.internetState),
-          );
-          }
-          print("objectfgkflkgdifoidfoid") ;
-          return Center(
-            child: Text(""),
-          );
-        },
-      ),
+      body: Column(children: [
+        ElevatedButton(onPressed: ()=> {}, child: Text("show")) ,
+        ElevatedButton(onPressed: ()=> {}, child: Text("show")) ,
+      ],)  ,
     );
   }
 }
