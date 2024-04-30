09724ca372a1a306be6e78de7525003503ef2f7d
Najm Al-Den <141837481+najm-flutter@users.noreply.github.com>
2024-04-30 16:32:39 +0300

    Delete lib/bloc directory

--- lib/bloc/connictivityBloc/connictivity_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'connictivity_bloc.dart';
-
-@immutable
-sealed class ConnictivityEvent {}
-
-class OfflineConnictivityEvent extends ConnictivityEvent{}
-class OnlineConnictivityEvent extends ConnictivityEvent{}
--- lib/bloc/themeBloc/theme_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'theme_bloc.dart';
-
-@immutable
-sealed class ThemeEvent {}
- class LightThemeEvent extends ThemeEvent {}
- class DarkThemeEvent extends ThemeEvent{}
